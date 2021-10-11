78f708dad52a8214b7aecb5bbd9d7654920531e1
Payson Holmes <paysonholmes@yahoo.com>
2021-10-11 04:28:43 -0700

    v1.0.0

---
+++ krnlss/About.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace krnlss
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void SourceCreatorLabel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discordapp.com/users/769317112602034177");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/channel/UCObungL0NYOc5kPUPB3ADKg");
+        }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You are already viewing the credits page.", "Already Viewing");
+        }
+    }
+}
---
+++ krnlss/krnlss.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using sxlib.Specialized;
+using sxlib;
+using System.IO;
+using System.Diagnostics;
+
+namespace krnlss
+{
+    public partial class krnlss : Form
+    {
+        public krnlss()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About aboutUI = new About();
+            aboutUI.ShowDialog();
+        }
+
+        private void killRobloxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process[] runningProcesses = Process.GetProcesses();
+            foreach (Process process in runningProcesses)
+            {
+                // now check the modules of the process
+                foreach (ProcessModule module in process.Modules)
+                {
+                    if (module.FileName.Equals("RobloxPlayerLauncher.exe"))
+                    {
+                        process.Kill();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No Roblox Processes Found!", "Error!");
+                  }
+                }
+            }
+        }
+
+        private void ScriptView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            fastColoredTextBox1.Text = File.ReadAllText($"./scripts/{ScriptView.SelectedNode}");
+        }
+
+        private void Injectbtn_Click(object sender, EventArgs e)
+        {
+            Functions.Lib.Attach();
+        }
+
+        private void Savebtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+        }
+
+        private void Openbtn_Click(object sender, EventArgs e)
+        {
+            if (Functions.OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    this.fastColoredTextBox1.Text = File.ReadAllText(Functions.OpenFile.FileName);
+                }
+                catch (Exception Error)
+                {
+                    MessageBox.Show("Error loading script!" + Error.Message);
+                }
+            }
+        }
+
+        private void Clearbtn_Click(object sender, EventArgs e)
+        {
+            this.fastColoredTextBox1.Text = "";
+        }
+
+        private void Executebtn_Click(object sender, EventArgs e)
+        {
+            string script = this.fastColoredTextBox1.Text;
+            Functions.Lib.Execute(script);
+        }
+
+        private void ScriptHub_Betabtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Script Hub is in the Development Page and some features may not work.", "Development Feature");
+            ScriptHub ScriptHub = new ScriptHub();
+            ScriptHub.ShowDialog();
+        }
+
+        private void scriptHubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gamesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scriptHubToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Script Hub In Development, click the space inbetween the Inject & Save File Buttons to test the beta version.", "Development Notice");
+        }
+
+        private void krnlss_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("KRNL UI for Synapse X Made By [YT]P-DennyGaming#0001", "Credits");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            About aboutUI = new About();
+            aboutUI.ShowDialog();
+        }
+
+        private void injectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Functions.Lib.Attach();
+        }
+    }
+}
